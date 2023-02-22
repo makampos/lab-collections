6b6b79109f2f312ef673ebb463966fb08fa8772a
Matheus <matheus.campos@edu.unifil.br>
2023-02-22 23:06:40 +0100

    commented to testing

--- src/ContractorCo/Models/Common/Operators.cs
+++ src/ContractorCo/Models/Common/Operators.cs
@@ -4,10 +4,10 @@ namespace Models.Common;
 
 public static class Operators
 {
-    public static IEnumerable<T> Once<T>(this IEnumerable<T> sequence) =>
-        new SinglePassSequence<T>(sequence);
-    
-    public static IEnumerable<string> ToGrid<T>(
-        this IEnumerable<T> sequence, int width, int gap)
-        => new GriFormatter<T>(sequence).Format(width, gap); 
+    // public static IEnumerable<T> Once<T>(this IEnumerable<T> sequence) =>
+    //     new SinglePassSequence<T>(sequence);
+    //
+    // public static IEnumerable<string> ToGrid<T>(
+    //     this IEnumerable<T> sequence, int width, int gap)
+    //     => new GriFormatter<T>(sequence).Format(width, gap); 
 }
