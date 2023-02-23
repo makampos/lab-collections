8bdcc0de09b1e4e4400d123c79e5cfdd8b97678a
Matheus <matheus.campos@edu.unifil.br>
2023-02-23 21:53:39 +0100

    implementing shuffle

--- src/ContractorCo/Console/Program.cs
+++ src/ContractorCo/Console/Program.cs
@@ -8,7 +8,15 @@ try
     // Linear sequence
     Workers.TestData.ToGrid(500, 2).WriteLines();
     
+    using IEnumerator<Worker> shuffler = Workers.TestData.BeginShuffle();
     
+    for (int i = 0; i < 5; i++)
+    {
+        shuffler.Iterate(). Take(5).ToGrid(500, 2).WriteLines();
+        shuffler.Reset();
+    }
+
+
 } catch(Exception e)
 {
     WriteLine($"ERROR: {e.Message}");
