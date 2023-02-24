cdbca61f648cd91fafe713bcbc01b3b7b3b96b39
Matheus <matheus.campos@edu.unifil.br>
2023-02-24 20:45:45 +0100

    added execution

--- src/ContractorCo/Console/Program.cs
+++ src/ContractorCo/Console/Program.cs
@@ -1,4 +1,4 @@
-﻿using Console;
+﻿using System.Diagnostics;
 using Models;
 using Models.Common;
 using Models.Common.Pagination;
@@ -9,20 +9,37 @@ try
     // Linear sequence
 
     #region Shuffle
-    Workers.TestData.ToGrid(500, 2).WriteLines();
-    
-    using IEnumerator<Worker> shuffler = Workers.TestData.BeginShuffle();
-    
-    for (int i = 0; i < 5; i++)
-    {
-        shuffler.Iterate(). Take(5).ToGrid(500, 2).WriteLines();
-        shuffler.Reset();
-    }
+
+    // Workers.TestData.ToGrid(500, 2).WriteLines();
+    //
+    // using IEnumerator<Worker> shuffler = Workers.TestData.BeginShuffle();
+    //
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     shuffler.Iterate(). Take(5).ToGrid(500, 2).WriteLines();
+    //     shuffler.Reset();
+    // }
     #endregion
 
-   
+    #region Pagination
 
-} catch(Exception e)
+    IPaginated<Worker> pages =
+        Workers.TestData.Paginate(Worker.RateComparer, 5);
+
+    Stopwatch pageTimer = Stopwatch.StartNew();
+    IEnumerator<IPage<Worker>> enumerator = pages.GetEnumerator();
+
+    while (enumerator.MoveNext())
+    {
+        IPage<Worker> page = enumerator.Current;
+        PayRate rate = page.AveragePayRate();
+        WriteLine($"Page #{page.Ordinal}: {page.Count}x{rate} [{pageTimer.Elapsed}]");
+        pageTimer.Restart();
+    }
+
+    #endregion
+}
+catch (Exception e)
 {
     WriteLine($"ERROR: {e.Message}");
 }
